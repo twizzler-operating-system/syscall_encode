a56c922f47a8b6c392b8fe64410c61630d4e9904
Daniel Bittman <danielbittman1@gmail.com>
2023-08-02 17:49:12 -0700

    Initial pass.

---
+++ syscall_macros/src/lib.rs
@@ -0,0 +1,12 @@
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+
+mod syscall;
+#[proc_macro_derive(SyscallSerialize, attributes(reg_bits, num_regs))]
+pub fn syscall_serialize_proc_macro(input: TokenStream) -> TokenStream {
+    let derive_input: DeriveInput = parse_macro_input!(input as DeriveInput);
+    match syscall::derive_proc_macro_impl(derive_input) {
+        Ok(ts) => ts.into(),
+        Err(err) => TokenStream::from(err.to_compile_error()),
+    }
+}
